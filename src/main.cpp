c5ad084d94d7f6dd6edde9ab5a35952668a07143
Stephan Unverwerth <s.unverwerth@gmx.de>
2020-08-28 14:49:58 +0200

    Set up Visual Studio project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#ifdef _WIN32
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+#else
+int main(int argc, char** argv) {
+#endif
+	return 0;
+}
